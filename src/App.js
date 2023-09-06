5a8c291bb927a7d0aa651cd1bb750bc75329d641
CodeSandbox <support@codesandbox.io>
2023-09-06 03:59:47 +0000

    Sandbox snapshot commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./styles.css";
+
+const randomQuestionsAndAnswers = [
+  {
+    title: "What is the capital of France?",
+    text: "The capital of France is Paris."
+  },
+  {
+    title: "How many continents are there?",
+    text: "There are seven continents on Earth."
+  },
+  {
+    title: "What is the largest planet in our solar system?",
+    text: "The largest planet in our solar system is Jupiter."
+  }
+];
+
+export default function App() {
+  
+  return (
+    <div className="App">
+      <Accordion questions={randomQuestionsAndAnswers} />
+    </div>
+  );
+}
+
+function Accordion({ questions }) {
+
+  const [curOpen,setCurOpen] = useState(null)
+
+  
+  return (
+    <div className="accordion">
+      {questions.map((el, id) => (
+        <AccordionItem curOpen={curOpen} onOpen={setCurOpen} key={id} num={id} title={el.title} text={el.text} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text,curOpen,onOpen }) {
+  const open = num===curOpen;
+  const handleToggle = ()=>{
+    onOpen(open? null: num)
+  }
+  
+  return (
+    <div className={open?"item box":"item"} onClick={handleToggle}>
+      <p className={open?"number active":"number"}>{num<9?`0${num}`:`${num}`}</p>
+      <p className={open?"title active":"title"}>{title}</p>
+      <p className={open?"text active":"text"}>{open?'-':'+'}</p>
+      {open && (<div className="content">{text}</div>)}
+    </div>
+  );
+}
